9f9331ca5937ca8f7e6925eb6b9462e729182919
mahabubr <mahabub.r753@gmail.com>
2022-10-26 19:30:26 +0600

    course details link created and vervel prod done

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000
 app.use(cors())
 
 const category = require('./data/category.json')
+const courseItem = require('./data/course.json')
 
 app.get('/', (req, res) => {
     res.send('Hello World')
@@ -14,6 +15,13 @@ app.get('/', (req, res) => {
 app.get('/category', (req, res) => {
     res.send(category)
 })
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id
+    const selectedItem = courseItem.find(item => item.category_id === id)
+    res.send(selectedItem)
+})
+
 
 app.listen(port, () => {
     console.log(`port running on ${port}`);
