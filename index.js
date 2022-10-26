4450d06704b1147e2b09388b2472ac732863e91b
mahabubr <mahabub.r753@gmail.com>
2022-10-27 00:39:48 +0600

    check out page data done

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(cors())
 
 const category = require('./data/category.json')
 const courseItem = require('./data/course.json')
+const checkout = require('./data/checkout.json')
 
 app.get('/', (req, res) => {
     res.send('Hello World')
@@ -19,6 +20,11 @@ app.get('/category', (req, res) => {
 app.get('/category/:id', (req, res) => {
     const id = req.params.id
     const selectedItem = courseItem.find(item => item.category_id === id)
+    res.send(selectedItem)
+})
+app.get('/checkout/:id', (req, res) => {
+    const id = req.params.id
+    const selectedItem = checkout.find(item => item.id === id)
     res.send(selectedItem)
 })
 
